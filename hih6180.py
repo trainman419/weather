a304e605267d495cee45257549d84beed758ff96
trainman419 <legotown@aol.com>
2020-05-03 05:22:35 +0000

    Temperature reading working

--- hih6180.py
+++ hih6180.py
@@ -5,6 +5,7 @@
 # Author: Austin Hendrix
 #
 
+import time
 import board
 import busio
 
@@ -23,7 +24,33 @@ class HIH6180(object):
 if __name__ == "__main__":
     bus = busio.I2C(board.SCL, board.SDA)
 
+    device = i2c_device.I2CDevice(bus, 0x27)
 
-    device = bus.I2CDevice(bus, 0x27)
+    out = bytearray(0)
 
+    device.write(out)
+
+    time.sleep(0.1)
+
+    bytes_read = bytearray(4)
+
+    device.readinto(bytes_read)
+
+    print("Raw data", bytes_read[0], bytes_read[1], bytes_read[2], bytes_read[3])
+
+    status = (bytes_read[0] & 0xC0) >> 6
+    print("Status", status)
+
+    humidity = ((bytes_read[0] & 0x03) << 8) | (bytes_read[1])
+    print(humidity, (1<<14))
+    humidity = (humidity / ((1<<14) - 2))
+    print("Humidity: {:.2f} %RH".format(humidity * 100.0))
+
+    temperature = (bytes_read[2] << 6) | (bytes_read[3] >> 2)
+
+    print(temperature)
+
+    temperature = (temperature / ((1<<14) - 2)) * 165 - 40
+
+    print("Temperature: {:.2f} C".format(temperature))
 
