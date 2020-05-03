bb182927bf81cebe7be16b4d81fcec2e0d7d61ab
trainman419 <legotown@aol.com>
2020-05-03 06:00:45 +0000

    Refactor into proper class

--- hih6130.py
+++ hih6130.py
@@ -19,38 +19,37 @@ except ImportError:
 
 
 class HIH6130(object):
-    pass
+
+    def __init__(self, bus):
+        self._dev = i2c_device.I2CDevice(bus, 0x27)
+
+    def read(self):
+        # Start reading; send write without any data.
+        self._dev.write(bytearray(0))
+
+
+        status = 1
+        bytes_read = bytearray(4)
+
+        while status != 0:
+            time.sleep(0.01)
+            self._dev.readinto(bytes_read)
+            status = (bytes_read[0] & 0xC0) >> 6
+
+        humidity = ((bytes_read[0] & 0x3F) << 8) | (bytes_read[1])
+        humidity = (humidity / ((1<<14) - 2))
+
+        temperature = (bytes_read[2] << 6) | (bytes_read[3] >> 2)
+        temperature = (temperature / ((1<<14) - 2)) * 165 - 40
+
+        return humidity, temperature
+
 
 if __name__ == "__main__":
     bus = busio.I2C(board.SCL, board.SDA)
 
-    device = i2c_device.I2CDevice(bus, 0x27)
+    sensor = HIH6130(bus)
 
-    out = bytearray(0)
-
-    device.write(out)
-
-    time.sleep(0.05)
-
-    bytes_read = bytearray(4)
-
-    device.readinto(bytes_read)
-
-    print("Raw data", bytes_read[0], bytes_read[1], bytes_read[2], bytes_read[3])
-
-    status = (bytes_read[0] & 0xC0) >> 6
-    print("Status", status)
-
-    humidity = ((bytes_read[0] & 0x3F) << 8) | (bytes_read[1])
-    print(humidity, (1<<14))
-    humidity = (humidity / ((1<<14) - 2))
+    humidity, temperature = sensor.read()
     print("Humidity: {:.2f} %RH".format(humidity * 100.0))
-
-    temperature = (bytes_read[2] << 6) | (bytes_read[3] >> 2)
-
-    print(temperature)
-
-    temperature = (temperature / ((1<<14) - 2)) * 165 - 40
-
     print("Temperature: {:.2f} C".format(temperature))
-
